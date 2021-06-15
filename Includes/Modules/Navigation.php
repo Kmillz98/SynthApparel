df889b140b456fd2d63d3c867744993e45edb156
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-15 22:50:16 +0000

    Added nav items

--- Includes/Modules/Navigation.php
+++ Includes/Modules/Navigation.php
@@ -7,5 +7,7 @@
   <div class="navbar-menu" id="mainNavMenu">
     <a class="navbar-item" href="/index.php">Home</a>
     <a class="navbar-item" href="#">T-Shirts</a>
+    <a class="navbar-item" href="#">Jumpers</a>
+    <a class="navbar-item" href="#">Accessories</a>
   </div>
 </nav>
