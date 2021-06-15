5e32775af6b6f69e2b6e2b24a2fb375e720d0610
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-15 21:49:56 +0000

    Setup Modules

---
+++ Includes/Modules/Header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Synth Apparel</title>
+
+		<!-- Meta Tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="node_modules/bulma/css/bulma.min.css" />
+	</head>
+	<body>
--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Synth Apparel</title>
+<?php include_once("Header.php"); ?>
 
-		<!-- Meta Tags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<!-- Stylesheets -->
-		<link rel="stylesheet" type="text/css" href="node_modules/bulma/css/bulma.min.css" />
-	</head>
-	<body>
-		<div class="buttons">
-			<button class="button is-primary">Primary</button>
-			<button class="button is-link">Link</button>
-		</div>
-
-		<div class="buttons">
-			<button class="button is-info">Info</button>
-			<button class="button is-success">Success</button>
-			<button class="button is-warning">Warning</button>
-			<button class="button is-danger">Danger</button>
-		</div>
-	</body>
-</html>
+<?php include_once("Footer.php"); ?>
