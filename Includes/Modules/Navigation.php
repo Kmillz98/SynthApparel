6ba4e9185558b203eecf6d137afd5bc00b9adb71
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-15 22:19:03 +0000

    Adding color to the navbar

--- Includes/Modules/Navigation.php
+++ Includes/Modules/Navigation.php
@@ -1,10 +1,9 @@
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar" role="navigation" aria-label="main navigation" id="mainNavbar">
   <div class="navbar-brand">
     <!-- navbar items, navbar burger... -->
   </div>
-  <div class="navbar-menu" id="main-navigation">
+  <div class="navbar-menu" id="mainNavMenu">
     <a class="navbar-item" href="/index.php">Home</a>
     <a class="navbar-item" href="#">T-Shirts</a>
-
   </div>
 </nav>
