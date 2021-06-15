24964305fad50319a193f4b2c37f63271987fea6
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-15 22:08:29 +0000

    Testing nav

--- Includes/Modules/Navigation.php
+++ Includes/Modules/Navigation.php
@@ -1,29 +1,10 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
-  <!-- Navigation Icons -->
   <div class="navbar-brand">
-    <!-- Navigation Icon (Brand Logo) -->
-    <a class="navbar-item" href="/index.php">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
-
-    <!-- Hamburger Icon -->
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="mainNavBar">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
+    <!-- navbar items, navbar burger... -->
   </div>
-
-  <!-- Navigation Menu -->
-  <div id="mainNavBar" class="navbar-menu">
+  <div class="navbar-menu">
     <div class="navbar-start">
-      <!-- Landing Page -->
-      <a class="nav-item" href="/index.php">Home</a>
-
-      <!-- Store Pages -->
-
-      <!-- Static Pages-->
-      
+      <a class="navbar-link">Home</a>
     </div>
   </div>
 </nav>
