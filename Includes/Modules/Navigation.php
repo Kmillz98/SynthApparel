d46f231e2bdb3e61c3b17a97de2f5976c659c631
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-15 22:01:23 +0000

    Setup Navigation bar

---
+++ Includes/Modules/Navigation.php
@@ -0,0 +1,29 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <!-- Navigation Icons -->
+  <div class="navbar-brand">
+    <!-- Navigation Icon (Brand Logo) -->
+    <a class="navbar-item" href="/index.php">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <!-- Hamburger Icon -->
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="mainNavBar">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <!-- Navigation Menu -->
+  <div id="mainNavBar" class="navbar-menu">
+    <div class="navbar-start">
+      <!-- Landing Page -->
+      <a class="nav-item" href="/index.php">Home</a>
+
+      <!-- Store Pages -->
+
+      <!-- Static Pages-->
+      
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php include_once("Includes/Modules/Header.php"); ?>
+<?php include_once("Includes/Modules/Navigation.php"); ?>
+
+<!-- Content Goes Here -->
 
 <?php include_once("Includes/Modules/Footer.php"); ?>
