25ed62c54b9642b2d337a224b28b2435ef2cf960
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-15 21:42:58 +0000

    Added Initial Template

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<h1>New Test 2</h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Synth Apparel</title>
+
+		<!-- Meta Tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="node_modules/bulma/bulma.min.css" />
+	</head>
+	<body>
+		<section class="section">
+			<div class="container">
+				<h1 class="title">
+					Hello World
+				</h1>
+				<p class="subtitle">
+					My first website with <strong>Bulma</strong>!
+				</p>
+			</div>
+		</section>
+	</body>
+</html>
