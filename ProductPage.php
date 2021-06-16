ef401e483cefb467259c32936c09d46e8690a92b
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-16 20:37:43 +0000

    Added product title

--- ProductPage.php
+++ ProductPage.php
@@ -6,5 +6,8 @@
   <div class="column is-narrow">
     <img class="main-product-img" src="/Images/Nav-logo.png" />
   </div>
+  <div class="column" id="productDetails">
+    <h2 class="product-title">Synth Apparel Original Collection</h2>
+  </div>
 </div>
 <?php include_once("Includes/Modules/Footer.php"); ?>
