16eee325c2297b38fa4ef5677e6fe78869d9e6ad
K.A.Miller@outlook.com <s200322@students.aie.edu.au>
2021-06-16 17:30:40 +1000

    Added Button

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 
 <!-- Content Goes Here -->
 
-<!-- Jumbotron -->
+<!-- Hero --> 
+<!-- Hero Image: https://unsplash.com/photos/7pq_V5KGHCc?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink -->
 <section class="hero is-large is-link" id="landingHero">
   <div class="hero-body">
-
+    <center>
+      <button class="is-primary">Shop Now</button>
+    </center>
   </div>
 </section>
 
