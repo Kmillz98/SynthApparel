123cfe3d31858e48af82dd7e7aa4c6034814935d
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-16 00:03:56 +0000

    Added Hero

--- Includes/Modules/Navigation.php
+++ Includes/Modules/Navigation.php
@@ -1,13 +1,19 @@
 <nav class="navbar" role="navigation" aria-label="main navigation" id="mainNavbar">
+  <!-- Navigation Logo -->
   <div class="navbar-brand">
     <a class="nav-item" id="navbarLogo">
       <img src="/Images/Nav-logo.png" alt="Synth Apparel" id="navbarIcon" />
     </a>
   </div>
+  <!-- Navigation Menus-->
   <div class="navbar-menu" id="mainNavMenu">
+    <!-- Landing Page -->
     <a class="navbar-item" href="/index.php">Home</a>
+    <!-- Store Pages / Dynamic Pages -->
     <a class="navbar-item" href="#">T-Shirts</a>
     <a class="navbar-item" href="#">Jumpers</a>
     <a class="navbar-item" href="#">Accessories</a>
+
+    <!-- Static Pages -->
   </div>
 </nav>
--- index.php
+++ index.php
@@ -3,4 +3,11 @@
 
 <!-- Content Goes Here -->
 
+<!-- Jumbotron -->
+<section="hero is-primary is-medium">
+  <div class="hero-body">
+
+  </div>
+</section>
+
 <?php include_once("Includes/Modules/Footer.php"); ?>
