c719a9fc9bdbcb8804c93ba1ad4a1b673ce1715d
K.A.Miller@outlook.com <s200322@students.aie.edu.au>
2021-06-16 18:06:24 +1000

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 <!-- Hero --> 
 <!-- Hero Image: https://unsplash.com/photos/7pq_V5KGHCc?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink -->
 <section class="hero is-large is-link" id="landingHero">
+  <div class="hero-body">
+
+  </div>
 </section>
 
 <?php include_once("Includes/Modules/Footer.php"); ?>
