68f60862cb90b82c9e59bb6ed0f317987d33b19d
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-16 20:29:41 +0000

    Adding a product image

---
+++ ProductPage.php
@@ -0,0 +1,10 @@
+<?php include_once("Includes/Modules/Header.php"); ?>
+<?php include_once("Includes/Modules/Navigation.php"); ?>
+
+<!-- Content -->
+<div class="columns">
+  <div class="column is-narrow">
+    <img class="main-product-img" src="" />
+  </div>
+</div>
+<?php include_once("Includes/Modules/Footer.php"); ?>
