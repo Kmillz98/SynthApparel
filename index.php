f6da95f37fb5d04ec4324a9f167afa69d4a257bd
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-16 20:14:43 +0000

    No Idea What's doing

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!--<?php include_once("Includes/Modules/Header.php"); ?> -->
+<<?php include_once("Includes/Modules/Navigation.php"); ?>
 
 <!-- Content Goes Here -->
 
@@ -8,3 +10,5 @@
 
   </div>
 </section>
+
+<?php include_once("Includes/Modules/Footer.php"); ?>
