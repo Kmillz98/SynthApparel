af5f5ce1ea60a2ee9ea5a8f7fcab42729d4711fc
Kmillz98 <K.A.Miller@OUTLOOK.COM>
2021-06-16 23:12:59 +0000

    Adding item details

--- ProductPage.php
+++ ProductPage.php
@@ -8,6 +8,20 @@
   </div>
   <div class="column" id="productDetails">
     <h2 class="product-title">Synth Apparel Original Collection</h2>
+    <br />
+    <div class="product-notes">
+      <ul>
+        <li>Preshrunk Jersey Knit</li>
+        <li>Semi Fitted Style</li>
+        <li>100% Cotton</li>
+      </ul>
+      <br />
+      <span id="sizeInfoHeading"><h4>How to pick the right size?</h4></span>
+      <p id="sizeInfo">
+        Get your favorite shirt, lay it flat on a table and measure it across the chest (one side only), and pick what size will fit best.
+Please note: measurements can vary within 2.5cm, this is within our tolerance.  
+      </p>
+    </div>
   </div>
 </div>
 <?php include_once("Includes/Modules/Footer.php"); ?>
